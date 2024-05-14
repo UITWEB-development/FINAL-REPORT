d8f67dea3cbbb9da3a629a3fd0311cbecbad6e9a
kakapt <phuocthinh.vu@gmail.com>
2024-05-14 20:39:58 +0700

    Add admin login view

--- routes/route-test.php
+++ routes/route-test.php
@@ -1,7 +1,11 @@
 <?php
 
+use App\Http\Controllers\AdminController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/login-header-test', function(){
     return view('login-header-test');
 });
+
+
+Route::get('/admin/login', [AdminController::class, 'login']);
