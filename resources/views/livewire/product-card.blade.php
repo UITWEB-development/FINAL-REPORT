438d3dfc2d10fae8abdfeb7f0f3dad466d3fbc78
Trung Tin <trungtin.le.2004@gmail.com>
2024-05-29 15:33:53 +0700

    style_delete_product and  remove_star

--- resources/views/livewire/product-card.blade.php
+++ resources/views/livewire/product-card.blade.php
@@ -1,21 +1,14 @@
 <nav class="grid gap-4 lg:gap-8 mt-4" >
     <a class=" group aspect-h-10 aspect-w-16 relative overflow-hidden rounded-2xl bg-black/25 transition hover:ring-4 hover:ring-[#cd853f] active:opacity-75">
         <img class="object-cover" src="{{ asset('storage/'.$product->image_path) }}"/>
-        <div class="cursor-pointer absolute inset-0 flex flex-col justify-between bg-gradient-to-b from-transparent via-black/75 to-black">
-            <div class="flex items-center justify-start gap-2 p-4">
-                <div class="flex items-center gap-1 rounded-lg bg-slate-800/50 px-1.5 py-1 font-medium text-slate-200">
-                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" class="hi-mini hi-star -mt-px inline-block h-4 w-4 text-amber-400">
-                        <path fill-rule="evenodd" d="M10.868 2.884c-.321-.772-1.415-.772-1.736 0l-1.83 4.401-4.753.381c-.833.067-1.171 1.107-.536 1.651l3.62 3.102-1.106 4.637c-.194.813.691 1.456 1.405 1.02L10 15.591l4.069 2.485c.713.436 1.598-.207 1.404-1.02l-1.106-4.637 3.62-3.102c.635-.544.297-1.584-.536-1.65l-4.752-.382-1.831-4.401z" clip-rule="evenodd"/>
-                    </svg>
-                    <span>6.7</span>
-                </div>
-            </div>
+        <div class="cursor-pointer absolute inset-0 flex  justify-between bg-gradient-to-b from-transparent  to-black/80">
+       
             <div class="flex items-end justify-between gap-2 px-4 py-5">
                 <div class="space-y-1">
                     <h3 class="text-lg font-semibold text-white ">{{ $product->name }}</h3>
                     <p class="text-sm font-semibold text-[#e1e0e2]">{{ $product->price }}</p>
                 </div>
-                <div wire:click="$dispatch('openModal', { component: 'edit-product' })" class="cursor-pointer flex h-10 w-10 items-center justify-center rounded-full bg-slate-500/25 text-slate-400 transition group-hover:scale-110 group-hover:bg-[#da9858] group-hover:text-white group-active:scale-100 ">
+                <div wire:click="$dispatch('openModal', { component: 'edit-product' })" class="cursor-pointer flex h-10 w-10 items-center justify-center rounded-full bg-slate-500/25 text-slate-400 transition group-hover:scale-110 group-hover:bg-[#da9858] group-hover:text-white group-active:scale-100 absolute bot-4 right-4">
                     <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="hi-mini hi-play h-5 w-5 translate-x-0.5">
                         <path stroke-linecap="round" stroke-linejoin="round" d="m16.862 4.487 1.687-1.688a1.875 1.875 0 1 1 2.652 2.652L10.582 16.07a4.5 4.5 0 0 1-1.897 1.13L6 18l.8-2.685a4.5 4.5 0 0 1 1.13-1.897l8.932-8.931Zm0 0L19.5 7.125M18 14v4.75A2.25 2.25 0 0 1 15.75 21H5.25A2.25 2.25 0 0 1 3 18.75V8.25A2.25 2.25 0 0 1 5.25 6H10" />
                     </svg>
