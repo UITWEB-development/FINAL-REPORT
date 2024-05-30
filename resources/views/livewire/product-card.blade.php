42b91f3f659fbd71ac928a74999f09e7d3a2bf3b
kakapt <phuocthinh.vu@gmail.com>
2024-05-30 09:40:57 +0700

    Add view product modal

--- resources/views/livewire/product-card.blade.php
+++ resources/views/livewire/product-card.blade.php
@@ -1,4 +1,4 @@
-<nav class="grid gap-4 lg:gap-8 mt-4" >
+<nav wire:click="$dispatch('openModal', { component: 'view-product', arguments: { product : '{{$product->id}}' }})" class="grid gap-4 lg:gap-8 mt-4" >
     <a class=" group aspect-h-10 aspect-w-16 relative overflow-hidden rounded-2xl bg-black/25 transition hover:ring-4 hover:ring-[#cd853f] active:opacity-75">
         <img class="object-cover" src="{{ asset('storage/'.$product->image_path) }}"/>
         <div class="cursor-pointer absolute inset-0 flex  justify-between bg-gradient-to-b from-transparent via-black/60 to-black">
@@ -10,10 +10,10 @@
                     <h3 class="text-lg font-semibold text-white ">{{ $product->name }}</h3>
                     <p class="text-sm font-semibold text-[#e1e0e2]">{{ $product->price }}</p>
                 </div>
-                <div wire:click="$dispatch('openModal', { component: 'edit-product', arguments: { product : '{{$product->id}}' }})" class="cursor-pointer flex h-10 w-10 items-center justify-center rounded-full bg-slate-500/25 text-slate-400 transition group-hover:scale-110 group-hover:bg-[#da9858] group-hover:text-white group-active:scale-100 absolute bot-4 right-4">
+                <div wire:click.stop="$dispatch('openModal', { component: 'edit-product', arguments: { product : '{{$product->id}}' }})" class="cursor-pointer flex h-10 w-10 items-center justify-center rounded-full bg-slate-500/25 text-slate-400 transition group-hover:scale-110 group-hover:bg-[#da9858] group-hover:text-white group-active:scale-100 absolute bot-4 right-4">
                     @svg('edit', 'hi-mini hi-play h-5 w-5')                      
                 </div>
-                <div wire:click="$dispatch('openModal', { component: 'delete-product', arguments: { product: '{{$product->id}}' }})" class="cursor-pointer flex h-10 w-10 items-center justify-center rounded-full bg-slate-500/25 text-slate-400 transition group-hover:scale-110 group-hover:bg-[#e81123] group-hover:text-white group-active:scale-100 absolute top-4 right-4">
+                <div wire:click.stop="$dispatch('openModal', { component: 'delete-product', arguments: { product: '{{$product->id}}' }})" class="cursor-pointer flex h-10 w-10 items-center justify-center rounded-full bg-slate-500/25 text-slate-400 transition group-hover:scale-110 group-hover:bg-[#e81123] group-hover:text-white group-active:scale-100 absolute top-4 right-4">
                     @svg('trash', 'hi-mini hi-play h-5 w-5')       
                 </div>
             </div>
