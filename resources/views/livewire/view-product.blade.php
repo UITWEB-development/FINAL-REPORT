5f201f87a27e9c4a1d83353c3fea9a0e82da2e7f
Trung Tin <trungtin.le.2004@gmail.com>
2024-05-30 14:35:18 +0700

    style view product user

--- resources/views/livewire/view-product.blade.php
+++ resources/views/livewire/view-product.blade.php
@@ -1,3 +1,73 @@
-<div>
-    Hello
+
+<div class="w-full max-w-6xl bg-white flex items-center p-5 lg:p-10 overflow-hidden relative">
+     
+    <div class="w-full max-w-6xl  bg-white  p-5  mx-auto text-gray-800 relative md:text-left">
+        <div class="md:flex items-center -mx-10">
+            {{-- ảnh sản phẩm --}}
+            <div class="w-full md:w-1/2 px-5   mb-10 md:mb-0">
+                <div class="relative">
+                    <img src="https://cdnmedia.thethaovanhoa.vn/Upload/qPf4BjfjvkrFearu8hrw/files/86289201_870689476727516_8388718750026694656_n.jpg" class="w-full relative z-10" alt="">
+                </div>
+            </div>
+            {{-- ảnh sản phẩm --}}
+            <div class="w-full md:w-1/2 px-5 pt-0">
+                <div class="mb-5">
+                    <div class="text-container overflow-y-auto max-h-52 overflow">
+                        <h1 class="font-bold uppercase text-2xl mb-5 ">
+                          Cơm sườn bì Lorem ipsum dolor sit amet consectetur adipisicing elit. Rerum?
+                        </h1>
+                 
+                        <p class="text-sm">
+                            inventore cupiditate.
+                        </p>
+                    </div>
+                </div>
+                <div class="pb-2">
+                    <div class="inline-block align-bottom mr-5">
+                        <span class="text-lg text-green-400 font-bold leading-none align-baseline">Available</span>
+                        <span class="text-lg text-red-400 font-bold leading-none align-baseline">Disavailable</span>
+                    </div>
+                </div>
+                <div>
+                    <div class="inline-block align-bottom mr-5">
+                        <span class="font-bold text-3xl leading-none align-baseline">5.000.000</span>
+                        <span class="text-xl font-bold leading-none align-baseline">VND</span>
+                    </div>
+                
+                </div>
+                <div class="pt-1">
+                    <div class="flex items-center justify-between pt-3">
+                        <div class="flex items-center border-gray-100">
+                            <span class="cursor-pointer rounded-l bg-gray-100 py-1 px-3.5 duration-100 hover:bg-blue-500 hover:text-blue-50"> - </span>
+                            <input class="no-spinners h-8 w-8 border bg-white text-center text-xs outline-none" type="number" value="2" min="1" />
+                            <span class="cursor-pointer rounded-r bg-gray-100 py-1 px-3 duration-100 hover:bg-blue-500 hover:text-blue-50"> + </span>
+                        </div>
+                        <a href="#"
+                            class="ml-1 custom-margin text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
+                            Add to cart
+                        </a>
+                    </div>
+                </div>
+                <style>
+                    .no-spinners::-webkit-outer-spin-button,
+                    .no-spinners::-webkit-inner-spin-button {
+                        -webkit-appearance: none;
+                        margin: 0;
+                    }
+                    
+                    .no-spinners {
+                        -moz-appearance: textfield;
+                    }
+                    
+                    /* Class for custom margin */
+                    .custom-margin {
+                        margin-left: 0.5rem; /* Adjust the value as needed */
+                    }
+                    </style>
+                
+            </div>
+        </div>
+    </div>
 </div>
+
+
