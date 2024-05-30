286dc9d29106be113c0c78d01d8e1acc01988393
Trung Tin <trungtin.le.2004@gmail.com>
2024-05-30 16:27:01 +0700

    style profile seller

--- resources/views/livewire/seller-dashboard.blade.php
+++ resources/views/livewire/seller-dashboard.blade.php
@@ -7,8 +7,12 @@
             }
         </script>
     @endif
-    <h2 class="mb-2 text-3xl font-semibold">Dashboard</h2>
+    {{-- <h2 class="mb-2 text-3xl font-semibold">Dashboard</h2>
     <h3 class="mb-8 text-sm font-medium text-slate-500">
         Welcome {{auth()->user()->name}}, everything looks great!
-    </h3>
+    </h3> --}}
+    
+
+    {{-- test display profile_seller --}}
+    <div class=""><livewire:seller-profile></livewire:seller-profile></div>
 </div>
---
+++ resources/views/livewire/seller-profile.blade.php
@@ -0,0 +1,73 @@
+<div>
+    <!-- component -->
+    <link rel="stylesheet" href="https://demos.creative-tim.com/notus-js/assets/styles/tailwind.css">
+    <link rel="stylesheet" href="https://demos.creative-tim.com/notus-js/assets/vendor/@fortawesome/fontawesome-free/css/all.min.css">
+
+    <section class="relative">
+        <div class="relative pt-16 pb-32 flex content-center items-center justify-center min-h-screen-75">
+            <div class="absolute top-0 w-full h-full bg-center bg-cover group" style="background-image: url('https://th.bing.com/th/id/OIP.ZJ1xzTKdVbk6dH-iN3kFOgHaE8?w=720&h=480&rs=1&pid=ImgDetMain');">
+                <div class="absolute inset-0 h-full w-full bg-black/65 flex flex-col justify-center items-center">
+                    <div class="relative text-center">
+                        <h2 class="block antialiased tracking-normal font-sans font-bold leading-[1.3] text-white mb-4 text-[5cqmin]">Lorem ipsum dolor sit, amet consectetur adipisicing elit.
+                        </h2>
+                    </div>
+                </div>
+                <div class="cursor-pointer flex h-10 w-10 items-center justify-center rounded-full bg-slate-500/25 text-white transition group-hover:scale-110 group-hover:bg-[#da9858] group-hover:text-white group-active:scale-100 absolute top-4 right-4">
+                    @svg('edit', 'hi-mini hi-play h-5 w-5')                      
+                </div>
+            </div>
+        </div>
+        <section class="-mt-24">
+            <div class="container mx-auto px-4">
+                <div class="flex flex-wrap">
+                    <div class="pt-6 w-full md:w-4/12 px-4 text-center group">
+                        <div class="relative flex flex-col min-w-0 break-words bg-white w-full mb-8 shadow-lg rounded-lg transition-transform transform group-hover:scale-105">
+                            <div class="px-4 py-5 flex-auto">
+                                <div class="text-white p-3 text-center inline-flex items-center justify-center w-12 h-12 mb-5 shadow-lg rounded-full bg-red-400">
+                                    @svg('fas-phone-volume')
+                                </div>
+                                <h6 class="text-xl font-semibold">Phone number</h6>
+                                <p class="mt-2 mb-4 text-blueGray-500">
+                                    0987654321
+                                </p>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="w-full md:w-4/12 px-4 text-center group">
+                        <div class="relative flex flex-col min-w-0 break-words bg-white w-full mb-8 shadow-lg rounded-lg transition-transform transform group-hover:scale-105">
+                            <div class="px-4 py-5 flex-auto">
+                                <div class="text-white p-3 text-center inline-flex items-center justify-center w-12 h-12 mb-5 shadow-lg rounded-full bg-lightBlue-400">
+                                    @svg('fas-map-location-dot')
+                                </div>
+                                <h6 class="text-xl font-semibold">Address</h6>
+                                <p class="mt-2 mb-4 text-blueGray-500">74b Hai Ba Trung, Ho Chi Minh, Viet Nam
+                                </p>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="pt-6 w-full md:w-4/12 px-4 text-center group">
+                        <div class="relative flex flex-col min-w-0 break-words bg-white w-full mb-8 shadow-lg rounded-lg transition-transform transform group-hover:scale-105">
+                            <div class="px-4 py-5 flex-auto">
+                                <div class="text-white p-3 text-center inline-flex items-center justify-center w-12 h-12 mb-5 shadow-lg rounded-full bg-emerald-400">
+                                    @svg('gmdi-timer')
+                                </div>
+                                <h6 class="text-xl font-semibold">Time</h6>
+                                <p class="mt-2 mb-4 text-blueGray-500">
+                                    09h00 - 23h00
+                                </p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+        <section class="text-gray-600 body-font relative">
+            <div class="absolute inset-0 bg-gray-300">
+              <iframe width="100%" height="100%" frameborder="0" marginheight="0" marginwidth="0" title="map" scrolling="no" src="https://maps.google.com/maps?width=100%&height=600&hl=en&q=%C4%B0zmir+(My%20Business%20Name)&ie=UTF8&t=&z=14&iwloc=B&output=embed" style=""></iframe>
+            </div>
+            <div class="container px-5 py-56 mx-auto flex">
+             
+            </div>
+          </section>
+    </section>
+</div>
