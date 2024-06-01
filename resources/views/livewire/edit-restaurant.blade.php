f29ae2160bd4554219bc78e1a46f222695330b12
Trung Tin <trungtin.le.2004@gmail.com>
2024-06-01 10:16:19 +0700

    remove address

--- resources/views/livewire/edit-restaurant.blade.php
+++ resources/views/livewire/edit-restaurant.blade.php
@@ -47,13 +47,6 @@
                 placeholder="Your Restaurant Name" required="">
         </div>
         <div>
-            <label for="address"
-                class="block mb-2 text-lg font-semibold text-gray-900 dark:text-white">Address</label>
-            <input type="text" name="address" id="address" 
-                class="mb-4 bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500"
-                placeholder="Address" required="">
-        </div>
-        <div>
             <label for="phone"
                 class="block mb-2 text-lg font-semibold text-gray-900 dark:text-white">Phone Number</label>
                 <input type="number" name="phone" id="phone"
