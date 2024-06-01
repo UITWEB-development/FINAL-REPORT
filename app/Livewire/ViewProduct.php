2e16a77f46f852fbfb3fd9b0fd2873f47850133c
kakapt <phuocthinh.vu@gmail.com>
2024-06-01 11:29:24 +0700

    finish product quick view

--- app/Livewire/ViewProduct.php
+++ app/Livewire/ViewProduct.php
@@ -2,10 +2,12 @@
 
 namespace App\Livewire;
 
+use App\Models\Product;
 use LivewireUI\Modal\ModalComponent;
 
 class ViewProduct extends ModalComponent
 {
+    public Product $product;
     public function render()
     {
         return view('livewire.view-product');
--- resources/views/livewire/view-product.blade.php
+++ resources/views/livewire/view-product.blade.php
@@ -1,70 +1,33 @@
 
 <div class="w-full max-w-6xl bg-white flex items-center p-5 lg:p-10 overflow-hidden relative">
-     
     <div class="w-full max-w-6xl  bg-white  p-5  mx-auto text-gray-800 relative md:text-left">
         <div class="md:flex items-center -mx-10">
-            {{-- ảnh sản phẩm --}}
             <div class="w-full md:w-1/2 px-5   mb-10 md:mb-0">
                 <div class="relative">
-                    <img src="https://cdnmedia.thethaovanhoa.vn/Upload/qPf4BjfjvkrFearu8hrw/files/86289201_870689476727516_8388718750026694656_n.jpg" class="w-full relative z-10" alt="">
+                    <img src="{{ asset('storage/'.$product->image_path) }}" class="max-h-72 w-auto relative z-10" alt="">
                 </div>
             </div>
-            {{-- ảnh sản phẩm --}}
             <div class="w-full md:w-1/2 px-5 pt-0">
                 <div class="mb-5">
-                    <div class="text-container overflow-y-auto max-h-52 overflow">
+                    <div class="text-container overflow-y-auto max-h-52 overflow-x-hidden">
                         <h1 class="font-bold uppercase text-2xl mb-5 ">
-                          Cơm sườn bì Lorem ipsum dolor sit amet consectetur adipisicing elit. Rerum?
+                          {{$product->name}}
                         </h1>
                  
-                        <p class="text-sm">
-                            inventore cupiditate. Lorem ipsum dolor sit amet consectetur adipisicing elit. Id facere in consequuntur at numquam! Cum hic voluptatibus rerum nam necessitatibus tempore alias facilis impedit eos iste provident, incidunt sit, obcaecati, at dignissimos ab quam praesentium molestiae ipsa commodi amet esse consectetur blanditiis! Cumque beatae expedita, necessitatibus facilis quo in magni, officiis eaque architecto fuga voluptatem harum ducimus atque eum fugit mollitia, distinctio culpa recusandae? Consectetur expedita dolorem possimus dolorum animi commodi, impedit aspernatur adipisci eum, voluptatem nisi nam libero. Praesentium commodi aut ipsa consectetur repellendus. Nemo suscipit placeat esse dicta nesciunt molestiae harum ea eius, similique veritatis eligendi, tenetur in!
-                        </p>
+                        <p class="text-sm">{{ $product->description }}</p>
                     </div>
                 </div>
                 <div class="pb-2">
                     <div class="inline-block align-bottom mr-5">
-                        <span class="text-lg text-green-400 font-bold leading-none align-baseline">Available</span>
-                        <span class="text-lg text-red-400 font-bold leading-none align-baseline">Disavailable</span>
+                        <span class="text-lg {{ $product->is_available ? 'text-green-400' : 'text-red-400'}} font-bold leading-none align-baseline">{{ $product->is_available ? 'Available' : 'Out of stock'}}</span>
                     </div>
                 </div>
                 <div>
                     <div class="inline-block align-bottom mr-5">
-                        <span class="font-bold text-3xl leading-none align-baseline">5.000.000</span>
+                        <span class="font-bold text-3xl leading-none align-baseline">{{$product->price}}</span>
                         <span class="text-xl font-bold leading-none align-baseline">VND</span>
                     </div>
-                
                 </div>
-                <div class="pt-1">
-                    <div class="flex items-center justify-between pt-3">
-                        <div class="flex items-center border-gray-100">
-                            <span class="cursor-pointer rounded-l bg-gray-100 py-1 px-3.5 duration-100 hover:bg-blue-500 hover:text-blue-50"> - </span>
-                            <input class="no-spinners h-8 w-8 border bg-white text-center text-xs outline-none" type="number" value="2" min="1" />
-                            <span class="cursor-pointer rounded-r bg-gray-100 py-1 px-3 duration-100 hover:bg-blue-500 hover:text-blue-50"> + </span>
-                        </div>
-                        <a href="#"
-                            class="ml-1 custom-margin text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
-                            Add to cart
-                        </a>
-                    </div>
-                </div>
-                <style>
-                    .no-spinners::-webkit-outer-spin-button,
-                    .no-spinners::-webkit-inner-spin-button {
-                        -webkit-appearance: none;
-                        margin: 0;
-                    }
-                    
-                    .no-spinners {
-                        -moz-appearance: textfield;
-                    }
-                    
-                    /* Class for custom margin */
-                    .custom-margin {
-                        margin-left: 0.5rem; /* Adjust the value as needed */
-                    }
-                    </style>
-                
             </div>
         </div>
     </div>
