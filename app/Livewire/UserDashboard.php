27ced930580a617aadf7bd37b9600eeb42bcaa4f
kakapt <phuocthinh.vu@gmail.com>
2024-06-04 09:53:45 +0700

    add user dashboard

--- app/Livewire/UserDashboard.php
+++ app/Livewire/UserDashboard.php
@@ -2,8 +2,20 @@
 
 namespace App\Livewire;
 
+use App\Constants\AuthStatusConstants;
+use App\Traits\UserTypeMount;
+use Illuminate\Support\Facades\Redirect;
+use Illuminate\Support\Facades\Validator;
+use Livewire\Attributes\Layout;
 use Livewire\Component;
+use Livewire\Attributes\Locked;
+use Livewire\Attributes\Title;
+use Illuminate\Validation\ValidationException;
+use Masmerise\Toaster\Toaster;
 
+
+#[Layout('components.layouts.user-dashboard')]
+#[Title('User Profile')] 
 class UserDashboard extends Component
 {
     public function render()
--- resources/views/livewire/user-dashboard.blade.php
+++ resources/views/livewire/user-dashboard.blade.php
@@ -1,4 +1,85 @@
 <div>
-    <p>Hello {{ auth()->user()->name}}</p>
-    <livewire:signout signin_route="/user/sign-in"></livewire:signout>
+    <div class="px-4 sm:px-20 z-10">
+        <div class="sm:flex sm:items-center sm:space-x-5 pt-10">
+            <div class="flex justify-center sm:justify-start">
+                <img class="h-24 w-24 rounded-full sm:h-32 sm:w-32" src="https://images.pexels.com/photos/56866/garden-rose-red-pink-56866.jpeg" alt="Profile Picture">
+            </div>
+            <div class="mt-6 sm:mt-0 sm:flex sm:items-center sm:space-x-6 sm:pb-1 flex-1">
+                <div class="min-w-0 flex-1" style="overflow: visible;">
+                    <h1 class="text-5xl font-bold text-black text-center sm:text-left">Tung Nguyen</h1>
+                </div>
+            </div>
+        </div>
+    
+        <div x-data="{ selectedTab: 'groups', open: false, hover: null }" class="w-full mt-6">
+            <!-- Tabs for larger screens -->
+            <div class="hidden sm:flex gap-2 overflow-x-auto border-b border-black" role="tablist" aria-label="tab options">
+                <button @click="selectedTab = 'groups'" :aria-selected="selectedTab === 'groups'" :tabindex="selectedTab === 'groups' ? '0' : '-1'" :class="selectedTab === 'groups' ? 'font-bold text-[#CD853F] border-b-2 border-[#CD853F] dark:border-[#CD853F] dark:text-[#CD853F]' : 'text-slate-700 font-medium dark:text-slate-300 dark:hover:border-b-slate-300 dark:hover:text-white hover:border-b-2 hover:border-b-slate-800 hover:text-black'" class="px-4 py-2 text-4xl" type="button" role="tab" aria-controls="tabpanelGroups">Profile</button>
+                <button @click="selectedTab = 'likes'" :aria-selected="selectedTab === 'likes'" :tabindex="selectedTab === 'likes' ? '0' : '-1'" :class="selectedTab === 'likes' ? 'font-bold text-[#CD853F] border-b-2 border-[#CD853F] dark:border-[#CD853F] dark:text-[#CD853F]' : 'text-slate-700 font-medium dark:text-slate-300 dark:hover:border-b-slate-300 dark:hover:text-white hover:border-b-2 hover:border-b-slate-800 hover:text-black'" class="px-4 py-2 text-4xl" type="button" role="tab" aria-controls="tabpanelLikes">Order History</button>
+            </div>
+    
+            <!-- Select for smaller screens -->
+            <div @click="open = !open" @keydown.escape="open = false" @click.away="open = false" class="relative sm:hidden mt-4">
+                <div class="block w-full border py-2 pl-1 border-[#CD853F] rounded-md shadow-sm cursor-pointer focus:ring-[#CD853F] focus:border-[#CD853F]">
+                    <span x-text="selectedTab === 'groups' ? 'Profile' : 'Order history'" class="block"></span>
+                </div>
+                <div x-show="open" class="absolute mt-1 w-full rounded-md bg-white shadow-lg z-10">
+                    <ul class="py-1 text-base leading-6 shadow-xs max-h-56 rounded-md ring-1 ring-black ring-opacity-5 overflow-auto focus:outline-none sm:text-sm sm:leading-5">
+                        <li @click="selectedTab = 'groups'; open = false" @mouseenter="hover = 'groups'" @mouseleave="hover = null" :class="{ 'bg-[#CD853F] text-black': selectedTab === 'groups' || hover === 'groups' }" class="bg-white hover:bg-[#CD853F] hover:text-white rounded-lg text-gray-900 cursor-pointer select-none relative py-2 pl-3 pr-9">
+                            <span class="font-normal block truncate">Profile</span>
+                        </li>
+                        <li @click="selectedTab = 'likes'; open = false" @mouseenter="hover = 'likes'" @mouseleave="hover = null" :class="{ 'bg-[#CD853F] text-black': selectedTab === 'likes' || hover === 'likes' }" class="bg-white hover:bg-[#CD853F] hover:text-white rounded-lg text-gray-900 cursor-pointer select-none relative py-2 pl-3 pr-9">
+                            <span class="font-normal block truncate">Order history</span>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+    
+            <!-- Content for each tab -->
+            <div class="my-10">
+                <!-- Profile Tab -->
+                <div x-show="selectedTab === 'groups'" id="tabpanelGroups" role="tabpanel" aria-label="groups" class="text-slate-700 dark:text-slate-300 flex justify-center bg-[#d9d8d6] rounded-lg relative p-10">
+                    <div class="w-full max-w-4xl py-14">
+                        <div class="space-y-5 ">
+                            <div class="relative p-4">
+                                <div class="flex flex-col sm:flex-row pb-4 md:space-x-60 text-2xl md:text-4xl">
+                                    <p class="font-bold sm:w-1/3">Name</p>
+                                    <p class="mt-1 sm:mt-0 sm:w-2/3 overflow-hidden text-ellipsis whitespace-nowrap">aaaaaaaaaaaa</p>
+                                </div>
+                                <div class="flex">
+                                    <span class="border-b border-slate-500 opacity-50 block w-full"></span>
+                                </div>
+                            </div>
+                            <div class="relative p-4">
+                                <div class="flex flex-col sm:flex-row pt-10 pb-4 md:space-x-60 text-2xl md:text-4xl">
+                                    <p class="font-bold sm:w-1/3">Email</p>
+                                    <p class="mt-1 sm:mt-0 sm:w-2/3 overflow-hidden text-ellipsis whitespace-nowrap">bbbbbbbbbbbbbbbbbbba</p>
+                                </div>
+                                <div class="flex">
+                                    <span class="border-b border-slate-500 opacity-50 block w-full"></span>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+    
+                    <div class="absolute top-0 right-0 m-6">
+                        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" class="h-6 w-6 text-black hover:text-[#CD853F] cursor-pointer">
+                            <path d="M21.731 2.269a2.625 2.625 0 0 0-3.712 0l-1.157 1.157 3.712 3.712 1.157-1.157a2.625 2.625 0 0 0 0-3.712ZM19.513 8.199l-3.712-3.712-8.4 8.4a5.25 5.25 0 0 0-1.32 2.214l-.8 2.685a.75.75 0 0 0 .933.933l2.685-.8a5.25 5.25 0 0 0 2.214-1.32l8.4-8.4Z" />
+                            <path d="M5.25 5.25a3 3 0 0 0-3 3v10.5a3 3 0 0 0 3 3h10.5a3 3 0 0 0 3-3V13.5a.75.75 0 0 0-1.5 0v5.25a1.5 1.5 0 0 1-1.5 1.5H5.25a1.5 1.5 0 0 1-1.5-1.5V8.25a1.5 1.5 0 0 1 1.5-1.5h5.25a.75.75 0 0 0 0-1.5H5.25Z" />
+                        </svg>
+                    </div>
+    
+    
+                </div>
+                </div>
+    
+                <!-- Order History Tab -->
+                <div x-show="selectedTab === 'likes'" id="tabpanelLikes" role="tabpanel" aria-label="likes" class="px-4 sm:px-10 md:px-20 lg:px-32 xl:px-52 py-10 w-full">
+                    <!-- Content for Order History -->
+                    <p>Order History here!</p>
+                </div>
+            </div>
+        </div>
+    </div>
+    
 </div>
