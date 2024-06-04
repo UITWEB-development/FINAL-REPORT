ba9d63750cba5f6938edb334e3ec47e462eb0732
kakapt <phuocthinh.vu@gmail.com>
2024-06-04 09:57:34 +0700

    add user dashboard layout

---
+++ resources/views/components/layouts/user-dashboard.blade.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>{{ $title ?? 'Default title' }}</title>
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Roboto+Slab&display=swap" rel="stylesheet">
+    @vite('resources/css/app.css')
+    @vite('resources/js/app.js')
+</head>
+
+<body class="font-roboto-slab">
+    <header >
+        @svg('gouchill', 'bi bi-window-sidebar inline-block h-20 w-20')
+        <livewire:signout signin_route="/user/sign-in"></livewire:signout>
+    </header>
+
+    {{ $slot }}
+    @livewire('wire-elements-modal')
+    <x-toaster-hub />
+</body>
+
+</html>
