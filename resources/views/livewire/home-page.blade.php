e6aec95332ee90fea874f8cf2d791bf14903fcde
Trung Tin <trungtin.le.2004@gmail.com>
2024-06-04 22:11:01 +0700

    add product home page

--- resources/views/livewire/home-page.blade.php
+++ resources/views/livewire/home-page.blade.php
@@ -1,3 +1,173 @@
 <div>
-    {{-- The Master doesn't talk, he acts. --}}
+    {{-- New restaurants --}}
+    <div class="w-full flex justify-between  border-black border-b-2 ">
+        <p class="text-[5cqmin] font-bold pl-4">New restaurants</p>
+        <a href="#" class="font-bold text-[3cqmin] pr-8 pt-5 hover:text-[#b17235] "> View All</a>
+    </div>
+        <nav class="p-5 gap-4  mt-4 grid grid-cols-1  sm:grid-cols-2 lg:grid-cols-3 lg:gap-8" >
+            <a class=" group aspect-h-10 aspect-w-16 relative overflow-hidden rounded-2xl bg-black/25 transition hover:ring-4 hover:ring-[#cd853f] active:opacity-75">
+                <img class="object-cover" src="https://th.bing.com/th/id/R.4038c562cfc67bf6421a148e7c4ac4d8?rik=Q%2f%2fOp4yxjuMcjg&pid=ImgRaw&r=0"/>
+                <div class="cursor-pointer absolute inset-0 flex  justify-between bg-gradient-to-b from-transparent  to-black ">
+                    <div class="flex items-end justify-between gap-2 px-4 py-5">
+                        <div class="cursor-pointer flex h-15 w-20 items-center justify-center rounded-full bg-slate-800 text-white transition group-hover:scale-110 group-hover:bg-yellow-100 group-hover:text-yellow-600 group-active:scale-100 absolute top-4 left-4">
+                   
+                                  <svg
+                                    xmlns="http://www.w3.org/2000/svg"
+                                    viewBox="0 0 20 20"
+                                    fill="currentColor"
+                                    class="hi-mini hi-star -mt-px inline-block h-10 w-6 text-amber-400"
+                                  >
+                                    <path
+                                      fill-rule="evenodd"
+                                      d="M10.868 2.884c-.321-.772-1.415-.772-1.736 0l-1.83 4.401-4.753.381c-.833.067-1.171 1.107-.536 1.651l3.62 3.102-1.106 4.637c-.194.813.691 1.456 1.405 1.02L10 15.591l4.069 2.485c.713.436 1.598-.207 1.404-1.02l-1.106-4.637 3.62-3.102c.635-.544.297-1.584-.536-1.65l-4.752-.382-1.831-4.401z"
+                                      clip-rule="evenodd"
+                                    />
+                                  </svg>
+                                  <span>8.6</span>
+                        </div>
+                        <div class="space-y-1 transition group-hover:scale-105 text-white group-hover:text-yellow-600 group-active:scale-100">
+                            <h3 class=" text-2xl font-semibold  ">Nhà hàng Vạn Huê Lầu</h3>
+                        </div>
+                    </div>
+                </div>
+            </a>
+            <a class=" group aspect-h-10 aspect-w-16 relative overflow-hidden rounded-2xl bg-black/25 transition hover:ring-4 hover:ring-[#cd853f] active:opacity-75">
+                <img class="object-cover" src="https://th.bing.com/th/id/R.4038c562cfc67bf6421a148e7c4ac4d8?rik=Q%2f%2fOp4yxjuMcjg&pid=ImgRaw&r=0"/>
+                <div class="cursor-pointer absolute inset-0 flex  justify-between bg-gradient-to-b from-transparent  to-black ">
+                    <div class="flex items-end justify-between gap-2 px-4 py-5">
+                        <div class="cursor-pointer flex h-15 w-20 items-center justify-center rounded-full bg-slate-800 text-white transition group-hover:scale-110 group-hover:bg-yellow-100 group-hover:text-yellow-600 group-active:scale-100 absolute top-4 left-4">
+                   
+                                  <svg
+                                    xmlns="http://www.w3.org/2000/svg"
+                                    viewBox="0 0 20 20"
+                                    fill="currentColor"
+                                    class="hi-mini hi-star -mt-px inline-block h-10 w-6 text-amber-400"
+                                  >
+                                    <path
+                                      fill-rule="evenodd"
+                                      d="M10.868 2.884c-.321-.772-1.415-.772-1.736 0l-1.83 4.401-4.753.381c-.833.067-1.171 1.107-.536 1.651l3.62 3.102-1.106 4.637c-.194.813.691 1.456 1.405 1.02L10 15.591l4.069 2.485c.713.436 1.598-.207 1.404-1.02l-1.106-4.637 3.62-3.102c.635-.544.297-1.584-.536-1.65l-4.752-.382-1.831-4.401z"
+                                      clip-rule="evenodd"
+                                    />
+                                  </svg>
+                                  <span>8.6</span>
+                        </div>
+                        <div class="space-y-1 transition group-hover:scale-105 text-white group-hover:text-yellow-600 group-active:scale-100">
+                            <h3 class=" text-2xl font-semibold  ">Nhà hàng Vạn Huê Lầu</h3>
+                        </div>
+                    </div>
+                </div>
+            </a>
+            <a class=" group aspect-h-10 aspect-w-16 relative overflow-hidden rounded-2xl bg-black/25 transition hover:ring-4 hover:ring-[#cd853f] active:opacity-75">
+                <img class="object-cover" src="https://th.bing.com/th/id/R.4038c562cfc67bf6421a148e7c4ac4d8?rik=Q%2f%2fOp4yxjuMcjg&pid=ImgRaw&r=0"/>
+                <div class="cursor-pointer absolute inset-0 flex  justify-between bg-gradient-to-b from-transparent  to-black ">
+                    <div class="flex items-end justify-between gap-2 px-4 py-5">
+                        <div class="cursor-pointer flex h-15 w-20 items-center justify-center rounded-full bg-slate-800 text-white transition group-hover:scale-110 group-hover:bg-yellow-100 group-hover:text-yellow-600 group-active:scale-100 absolute top-4 left-4">
+                   
+                                  <svg
+                                    xmlns="http://www.w3.org/2000/svg"
+                                    viewBox="0 0 20 20"
+                                    fill="currentColor"
+                                    class="hi-mini hi-star -mt-px inline-block h-10 w-6 text-amber-400"
+                                  >
+                                    <path
+                                      fill-rule="evenodd"
+                                      d="M10.868 2.884c-.321-.772-1.415-.772-1.736 0l-1.83 4.401-4.753.381c-.833.067-1.171 1.107-.536 1.651l3.62 3.102-1.106 4.637c-.194.813.691 1.456 1.405 1.02L10 15.591l4.069 2.485c.713.436 1.598-.207 1.404-1.02l-1.106-4.637 3.62-3.102c.635-.544.297-1.584-.536-1.65l-4.752-.382-1.831-4.401z"
+                                      clip-rule="evenodd"
+                                    />
+                                  </svg>
+                                  <span>8.6</span>
+                        </div>
+                        <div class="space-y-1 transition group-hover:scale-105 text-white group-hover:text-yellow-600 group-active:scale-100">
+                            <h3 class=" text-2xl font-semibold  ">Nhà hàng Vạn Huê Lầu</h3>
+                        </div>
+                    </div>
+                </div>
+            </a>
+        </nav>
+        {{-- Outstanding Restaurants --}}
+        <div class="w-full flex justify-between pt-10 border-black border-b-2 ">
+            <p class="text-[5cqmin] font-bold pl-4">Outstanding restaurants</p>
+        </div>
+            <nav class="p-5 gap-4  mt-4 grid grid-cols-1  sm:grid-cols-2 lg:grid-cols-3 lg:gap-8" >
+                <a class=" group aspect-h-10 aspect-w-16 relative overflow-hidden rounded-2xl bg-black/25 transition hover:ring-4 hover:ring-[#cd853f] active:opacity-75">
+                    <img class="object-cover" src="https://th.bing.com/th/id/R.4038c562cfc67bf6421a148e7c4ac4d8?rik=Q%2f%2fOp4yxjuMcjg&pid=ImgRaw&r=0"/>
+                    <div class="cursor-pointer absolute inset-0 flex  justify-between bg-gradient-to-b from-transparent  to-black ">
+                        <div class="flex items-end justify-between gap-2 px-4 py-5">
+                            <div class="cursor-pointer flex h-15 w-20 items-center justify-center rounded-full bg-slate-800 text-white transition group-hover:scale-110 group-hover:bg-yellow-100 group-hover:text-yellow-600 group-active:scale-100 absolute top-4 left-4">
+                       
+                                      <svg
+                                        xmlns="http://www.w3.org/2000/svg"
+                                        viewBox="0 0 20 20"
+                                        fill="currentColor"
+                                        class="hi-mini hi-star -mt-px inline-block h-10 w-6 text-amber-400"
+                                      >
+                                        <path
+                                          fill-rule="evenodd"
+                                          d="M10.868 2.884c-.321-.772-1.415-.772-1.736 0l-1.83 4.401-4.753.381c-.833.067-1.171 1.107-.536 1.651l3.62 3.102-1.106 4.637c-.194.813.691 1.456 1.405 1.02L10 15.591l4.069 2.485c.713.436 1.598-.207 1.404-1.02l-1.106-4.637 3.62-3.102c.635-.544.297-1.584-.536-1.65l-4.752-.382-1.831-4.401z"
+                                          clip-rule="evenodd"
+                                        />
+                                      </svg>
+                                      <span>8.6</span>
+                            </div>
+                            <div class="space-y-1 transition group-hover:scale-105 text-white group-hover:text-yellow-600 group-active:scale-100">
+                                <h3 class=" text-2xl font-semibold  ">Nhà hàng Vạn Huê Lầu</h3>
+                            </div>
+                        </div>
+                    </div>
+                </a>
+                <a class=" group aspect-h-10 aspect-w-16 relative overflow-hidden rounded-2xl bg-black/25 transition hover:ring-4 hover:ring-[#cd853f] active:opacity-75">
+                    <img class="object-cover" src="https://th.bing.com/th/id/R.4038c562cfc67bf6421a148e7c4ac4d8?rik=Q%2f%2fOp4yxjuMcjg&pid=ImgRaw&r=0"/>
+                    <div class="cursor-pointer absolute inset-0 flex  justify-between bg-gradient-to-b from-transparent  to-black ">
+                        <div class="flex items-end justify-between gap-2 px-4 py-5">
+                            <div class="cursor-pointer flex h-15 w-20 items-center justify-center rounded-full bg-slate-800 text-white transition group-hover:scale-110 group-hover:bg-yellow-100 group-hover:text-yellow-600 group-active:scale-100 absolute top-4 left-4">
+                       
+                                      <svg
+                                        xmlns="http://www.w3.org/2000/svg"
+                                        viewBox="0 0 20 20"
+                                        fill="currentColor"
+                                        class="hi-mini hi-star -mt-px inline-block h-10 w-6 text-amber-400"
+                                      >
+                                        <path
+                                          fill-rule="evenodd"
+                                          d="M10.868 2.884c-.321-.772-1.415-.772-1.736 0l-1.83 4.401-4.753.381c-.833.067-1.171 1.107-.536 1.651l3.62 3.102-1.106 4.637c-.194.813.691 1.456 1.405 1.02L10 15.591l4.069 2.485c.713.436 1.598-.207 1.404-1.02l-1.106-4.637 3.62-3.102c.635-.544.297-1.584-.536-1.65l-4.752-.382-1.831-4.401z"
+                                          clip-rule="evenodd"
+                                        />
+                                      </svg>
+                                      <span>8.6</span>
+                            </div>
+                            <div class="space-y-1 transition group-hover:scale-105 text-white group-hover:text-yellow-600 group-active:scale-100">
+                                <h3 class=" text-2xl font-semibold  ">Nhà hàng Vạn Huê Lầu</h3>
+                            </div>
+                        </div>
+                    </div>
+                </a>
+                <a class=" group aspect-h-10 aspect-w-16 relative overflow-hidden rounded-2xl bg-black/25 transition hover:ring-4 hover:ring-[#cd853f] active:opacity-75">
+                    <img class="object-cover" src="https://th.bing.com/th/id/R.4038c562cfc67bf6421a148e7c4ac4d8?rik=Q%2f%2fOp4yxjuMcjg&pid=ImgRaw&r=0"/>
+                    <div class="cursor-pointer absolute inset-0 flex  justify-between bg-gradient-to-b from-transparent  to-black ">
+                        <div class="flex items-end justify-between gap-2 px-4 py-5">
+                            <div class="cursor-pointer flex h-15 w-20 items-center justify-center rounded-full bg-slate-800 text-white transition group-hover:scale-110 group-hover:bg-yellow-100 group-hover:text-yellow-600 group-active:scale-100 absolute top-4 left-4">
+                       
+                                      <svg
+                                        xmlns="http://www.w3.org/2000/svg"
+                                        viewBox="0 0 20 20"
+                                        fill="currentColor"
+                                        class="hi-mini hi-star -mt-px inline-block h-10 w-6 text-amber-400"
+                                      >
+                                        <path
+                                          fill-rule="evenodd"
+                                          d="M10.868 2.884c-.321-.772-1.415-.772-1.736 0l-1.83 4.401-4.753.381c-.833.067-1.171 1.107-.536 1.651l3.62 3.102-1.106 4.637c-.194.813.691 1.456 1.405 1.02L10 15.591l4.069 2.485c.713.436 1.598-.207 1.404-1.02l-1.106-4.637 3.62-3.102c.635-.544.297-1.584-.536-1.65l-4.752-.382-1.831-4.401z"
+                                          clip-rule="evenodd"
+                                        />
+                                      </svg>
+                                      <span>8.6</span>
+                            </div>
+                            <div class="space-y-1 transition group-hover:scale-105 text-white group-hover:text-yellow-600 group-active:scale-100">
+                                <h3 class=" text-2xl font-semibold  ">Nhà hàng Vạn Huê Lầu</h3>
+                            </div>
+                        </div>
+                    </div>
+                </a>
+            </nav>
+        
+    
 </div>
