5740fc6795da45f99c670b00140de41d1a1d01e0
Trung Tin <trungtin.le.2004@gmail.com>
2024-06-05 16:16:34 +0700

    add style about-us

--- resources/views/livewire/about-us.blade.php
+++ resources/views/livewire/about-us.blade.php
@@ -1,3 +1,72 @@
-<div>
-    {{-- Because she competes with no one, no one can compete with her. --}}
-</div>
+
+<section class="">
+    <div class=" bg-white">
+        <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
+
+            <div class="lg:text-center">
+                <h2
+                    class="font-heading mb-4  text-[#a75b11] px-4 py-2 rounded-lg  md:mx-auto w-full text-[8cqmin] font-semibold tracking-widest  uppercase title-font">
+                    Why choose us?
+                </h2>
+                <p class="font-heading mt-2 text-3xl leading-8 font-semibold tracking-tight text-gray-900 sm:text-4xl">
+                    Taste - Ties - Treats
+                </p>
+                <p class="mt-4 max-w-2xl text-lg text-gray-500 lg:mx-auto">
+                    GOUCHILL is more than just an online food ordering platform. We are a community, a feeling and an experience.
+                </p>
+            </div>
+
+            <div class="mt-10">
+                <dl class="space-y-10 md:space-y-0 md:grid md:grid-cols-2 md:gap-x-8 md:gap-y-10">
+                    <div class="relative">
+                        <dt>
+                            <div>
+                                @svg('fluentui-task-list-square-person-20','absolute flex items-center justify-center h-12 w-12 rounded-md bg-primary-500 text-black')
+                            </div>
+                            <p class="font-heading ml-16 text-lg leading-6 font-bold text-gray-700">Mission</p>
+                        </dt>
+                        <dd class="mt-2 ml-16 text-base text-gray-500">
+                            Mission is to connect the community through flavors and culinary experiences. We believe that food is not only about eating, but also about creating memories, sharing, and connecting with each other.
+                        </dd>
+                    </div>
+                    <div class="relative">
+                        <dt>
+                            <div>
+                                @svg('gmdi-local-fire-department-r','absolute flex items-center justify-center h-12 w-12 rounded-md bg-primary-500 text-black')
+                            </div>
+                            <p class="font-heading ml-16 text-lg leading-6 font-bold text-gray-700">Passion
+                        </dt>
+                        <dd class="mt-2 ml-16 text-base text-gray-500"> At GOUCHILL, passion drives us forward. We are passionate about exploring and sharing delicious foods, as well as delivering the best experience to each of our users.
+                        </dd>
+                    </div>
+                    <div class="relative">
+                        <dt>
+                            <div>
+                                @svg('eos-shutter-speed','absolute flex items-center justify-center h-12 w-12 rounded-md bg-primary-500 text-black')
+
+                            </div>
+                            <p class="font-heading ml-16 text-lg leading-6 font-bold text-gray-700">Convenience and Speed
+                            </p>
+                        </dt>
+                        <dd class="mt-2 ml-16 text-base text-gray-500"> Ordering your favorite meals with GOUCHILL is not only simple, but also fast and convenient. With just a few clicks, you can enjoy a delicious meal from your favorite restaurant.
+                        </dd>
+                    </div>
+                    <div class="relative">
+                        <dt>
+                            <div>
+                                @svg('mdi-food','absolute flex items-center justify-center h-12 w-12 rounded-md bg-primary-500 text-black')
+
+                            </div>
+                            <p class="font-heading ml-16 text-lg leading-6 font-bold text-gray-700">One Experience, Many Flavors 
+                            </p>
+                        </dt>
+                        <dd class="mt-2 ml-16 text-base text-gray-500"> Join us in exploring the diverse world of culinary delights and creating unforgettable memories every day. GOUCHILL - Where connections are made through flavors!
+                        </dd>
+                    </div>
+                </dl>
+            </div>
+
+        </div>
+    </div>
+</section>
+
