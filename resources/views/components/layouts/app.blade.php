d79002daf4e7cfc623ae5e52b4871f1e7bd761cf
Trung Tin <trungtin.le.2004@gmail.com>
2024-06-06 11:00:33 +0700

    fix border color search

--- resources/views/components/layouts/app.blade.php
+++ resources/views/components/layouts/app.blade.php
@@ -22,12 +22,12 @@
         @svg('gouchill', 'h-10 w-36')
       </a>
       <!-- Search -->
-      <div class="relative flex mr-auto w-full max-w-64 flex-col gap-1 text-slate-700 dark:text-slate-300">
+      <div class="relative flex mr-auto w-full max-w-64 flex-col gap-1 text-slate-700 dark:text-slate-300 active:border-white">
         <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" aria-hidden="true" class="absolute left-2.5 top-1/2 size-5 -translate-y-1/2 text-slate-700/50 dark:text-slate-300/50"> 
-          <path stroke-linecap="round" stroke-linejoin="round" d="m21 21-5.197-5.197m0 0A7.5 7.5 0 1 0 5.196 5.196a7.5 7.5 0 0 0 10.607 10.607Z" />
+            <path stroke-linecap="round" stroke-linejoin="round" d="m21 21-5.197-5.197m0 0A7.5 7.5 0 1 0 5.196 5.196a7.5 7.5 0 0 0 10.607 10.607Z" />
         </svg>
-        <input type="search" name="search" placeholder="Search" aria-label="search" class="w-full rounded-xl border border-slate-300 bg-slate-100 py-2.5 pl-10 pr-2 text-sm focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-orange-500 disabled:cursor-not-allowed disabled:opacity-75 dark:border-slate-700 dark:bg-slate-800/50 dark:focus-visible:outline-orange-500" />
-      </div>
+        <input type="search" name="search" placeholder="Search" aria-label="search" class="w-full rounded-xl border border-slate-300 bg-slate-100 py-2.5 pl-10 pr-2 text-sm focus:border-[#c8835f] focus:ring focus:ring-[#ea580c] focus:ring-opacity-50 disabled:cursor-not-allowed disabled:opacity-75 dark:border-slate-700 dark:bg-slate-800/50" />
+    </div>
       <!-- Desktop Menu -->
       <ul class="hidden items-center gap-4 flex-shrink-0 sm:flex">
         <li><a href="/" class="font-bold text-orange-600 underline-offset-2 hover:text-orange-600 focus:outline-none focus:underline dark:text-orange-600 dark:hover:text-orange-600" aria-current="page">Home</a></li>
