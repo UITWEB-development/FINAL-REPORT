2fb74fc1ff8cfede0bcbf73ed0a6527a70b62f98
Trung Tin <trungtin.le.2004@gmail.com>
2024-06-12 15:11:04 +0700

    add avatar restaurant

--- resources/views/livewire/restaurant-google-map.blade.php
+++ resources/views/livewire/restaurant-google-map.blade.php
@@ -29,7 +29,7 @@
             <div class="  justify-between items-center w-full">
                 <div class="flex flex-row gap-3 items-center">
                     <!-- Phone icon -->
-                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" class="size-6">
+                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" class="size-5">
                         <path fill-rule="evenodd" d="M1.5 4.5a3 3 0 0 1 3-3h1.372c.86 0 1.61.586 1.819 1.42l1.105 4.423a1.875 1.875 0 0 1-.694 1.955l-1.293.97c-.135.101-.164.249-.126.352a11.285 11.285 0 0 0 6.697 6.697c.103.038.25.009.352-.126l.97-1.293a1.875 1.875 0 0 1 1.955-.694l4.423 1.105c.834.209 1.42.959 1.42 1.82V19.5a3 3 0 0 1-3 3h-2.25C8.552 22.5 1.5 15.448 1.5 6.75V4.5Z" clip-rule="evenodd" />
                       </svg>
                       
@@ -37,7 +37,7 @@
                 </div>
                 <div class=" flex gap-3 items-center ml-auto">
                     <!-- Clock icon -->
-                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" class="size-6">
+                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" class="size-5">
                         <path fill-rule="evenodd" d="M12 2.25c-5.385 0-9.75 4.365-9.75 9.75s4.365 9.75 9.75 9.75 9.75-4.365 9.75-9.75S17.385 2.25 12 2.25ZM12.75 6a.75.75 0 0 0-1.5 0v6c0 .414.336.75.75.75h4.5a.75.75 0 0 0 0-1.5h-3.75V6Z" clip-rule="evenodd" />
                       </svg>
                       
@@ -48,9 +48,10 @@
             <div class=" flex  lg:flex-row lg:justify-between mt-2 lg:mt-0">
                 <div class="flex flex-row gap-3 ">
                     <!-- Location icon -->
-                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" class="size-6">
-                        <path fill-rule="evenodd" d="m11.54 22.351.07.04.028.016a.76.76 0 0 0 .723 0l.028-.015.071-.041a16.975 16.975 0 0 0 1.144-.742 19.58 19.58 0 0 0 2.683-2.282c1.944-1.99 3.963-4.98 3.963-8.827a8.25 8.25 0 0 0-16.5 0c0 3.846 2.02 6.837 3.963 8.827a19.58 19.58 0 0 0 2.682 2.282 16.975 16.975 0 0 0 1.145.742ZM12 13.5a3 3 0 1 0 0-6 3 3 0 0 0 0 6Z" clip-rule="evenodd" />
+                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" class="size-5">
+                        <path fill-rule="evenodd" d="M8.161 2.58a1.875 1.875 0 0 1 1.678 0l4.993 2.498c.106.052.23.052.336 0l3.869-1.935A1.875 1.875 0 0 1 21.75 4.82v12.485c0 .71-.401 1.36-1.037 1.677l-4.875 2.437a1.875 1.875 0 0 1-1.676 0l-4.994-2.497a.375.375 0 0 0-.336 0l-3.868 1.935A1.875 1.875 0 0 1 2.25 19.18V6.695c0-.71.401-1.36 1.036-1.677l4.875-2.437ZM9 6a.75.75 0 0 1 .75.75V15a.75.75 0 0 1-1.5 0V6.75A.75.75 0 0 1 9 6Zm6.75 3a.75.75 0 0 0-1.5 0v8.25a.75.75 0 0 0 1.5 0V9Z" clip-rule="evenodd" />
                       </svg>
+                      
                       
                     <span class="text-[3cqmin]"> {{ $restaurant->restaurant_description->address }}</span>
                 </div>
--- resources/views/livewire/restaurant-page.blade.php
+++ resources/views/livewire/restaurant-page.blade.php
@@ -2,14 +2,22 @@
     <div class="py-5 relative">
         <div class="w-full px-4 md:px-5 l mx-auto">
             <div class="rounded-3xl p-4 lg:p-3 mb-2 max-lg:w-full max-lg:mx-auto ">
-                <h1 class="text-center mt-2 font-bold whitespace-nowrap text-[5cqmin]">
-                    {{ $restaurant->restaurant_description->restaurant_name }}</h1>
-               
                 <div
                     class="w-full flex-col lg:flex-row lg:items-center gap-10 mb-1 mx-2 2xl:mt-0 xl:mt-2 lg:mt-1 mt-3 md:flex-col">
-                    <div class="flex flex-row gap-3 items-center justify-center ml-auto">
-                            <a wire:click.prevent.stop="$dispatch('openModal', { component: 'restaurant-google-map', arguments: { restaurant : '{{$restaurant->id}}' }})" href="" class="text-[3cqmin] underline hover:text-orange-600 active:text-orange-600 ">View Detail</a>
+                    <div class="flex flex-row gap-3 items-center ml-auto">
+                        <img src="{{ asset('storage/' . $restaurant->image_path) }}" alt="speaker image"
+                            class="w-full max-w-[60px] h-auto sm:max-w-[80px] sm:h-[80px] md:max-w-[100px] md:h-[100px] lg:max-w-[120px] lg:h-[120px] rounded-full">
+                        <div>
+                            <h1
+                                class="text-center mt-2 font-bold whitespace-nowrap text-[4cqmin] sm:text-[3cqmin] md:text-[4cqmin] lg:text-[5cqmin]">
+                                {{ $restaurant->restaurant_description->restaurant_name }}</h1>
+
+                            <a wire:click.prevent.stop="$dispatch('openModal', { component: 'restaurant-google-map', arguments: { restaurant : '{{$restaurant->id}}' }})"
+                                href=""
+                                class="text-[2.8cqmin] underline text-gray-400 hover:text-orange-600 active:text-orange-600">View
+                                details</a>
                         </div>
+                    </div>
                 </div>
             </div>
         </div>
@@ -28,3 +36,4 @@
 
     <livewire:restaurant-product-list :$restaurant></livewire:restaurant-product-list>
 </div>
+
